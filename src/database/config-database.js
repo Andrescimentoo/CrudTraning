2a222fd9297264c267c02a75394e25bbb2414ad2
Andre Nascimento <1914andre@gmail.com>
2025-01-11 15:49:23 -0300

    feat: instantiating sequelize

---
+++ src/database/config-database.js
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("Atributes_players", "root","A19551947b"{
+    host:"localhost",
+    dialect: "mysql"
+})
+
+export{sequelize}
