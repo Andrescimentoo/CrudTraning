271370efbc65568f8f0857dbae6d7d4b6fc95e3f
Andre Nascimento <1914andre@gmail.com>
2025-01-11 16:17:03 -0300

    feat: creating and adding in project the model with ORM sequelize

---
+++ src/model/model.js
@@ -0,0 +1,35 @@
+import { DataTypes,} from "sequelize"; 
+import { database } from "../database/config-database";
+
+const characthers = database.define("personagens",{
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    
+    nome:{
+       type: DataTypes.STRING,
+       allowNull:false
+    },
+
+    idade: {
+       type: DataTypes.STRING,
+       allowNull:false
+    },
+    
+    poderes: {
+        type: DataTypes.STRING,
+        allowNull:false
+     },
+     raca: {
+        type: DataTypes.STRING,
+        allowNull:false
+     }
+
+    
+
+})
+
+export{characthers}
