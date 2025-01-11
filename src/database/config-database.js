6c4925f246945d609b5b9dc84cd0fdf30028c58e
Andre Nascimento <1914andre@gmail.com>
2025-01-11 16:15:59 -0300

    fix: rename the const to the sequelize

--- src/database/config-database.js
+++ src/database/config-database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("Atributes_players", "root","A19551947b"{
+const database = new Sequelize("Atributes_players", "root","A19551947b"{
     host:"localhost",
     dialect: "mysql"
 })
 
-export{sequelize}
+export{database}
