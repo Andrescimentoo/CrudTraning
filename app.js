fdec1197b818d003a81c27d7fb4df3a67a303306
Andre Nascimento <1914andre@gmail.com>
2025-01-13 17:03:10 -0300

    feat: addingrouteGet to aplication

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express"
+import { routeGet } from "./src/routes/get.Routes.js"
 
 const app = express()
 const Port = 3000
+
+app.use(routeGet)
 
 app.listen(Port,() => {
     console.log("server started!") 
