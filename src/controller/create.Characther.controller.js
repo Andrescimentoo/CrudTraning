65947ea0f84d7587d206a746e46ed9c38b210a92
Andre Nascimento <1914andre@gmail.com>
2025-01-20 16:22:19 -0300

    fix: altering name the controller for createCharacther

---
+++ src/controller/create.Characther.controller.js
@@ -0,0 +1,46 @@
+import { characthers } from "../model/model.js"
+
+const createCharacther = async (req,res) => {
+    try {
+        const createCharacther = await characthers.create(req.body) // atualiza pelo corpo da requisiçao 
+        
+        if (!createCharacther) {
+            res.status(404).send({message:"personagem nao encontrado!"})
+        } else {
+            res.status(201).send(createCharacther)
+        }
+    } catch (error) {
+        res.status(500).send(error)
+        console.log("erro ao criar personagem!",error)
+    }  
+
+}
+export{createCharacther}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
